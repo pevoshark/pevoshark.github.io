5c44d8ed30667d711491e6d0b63f4d795a87a453
pevoshark <arseniysdombrowski@gmail.com>
2023-05-11 11:36:51 +0500

    ratata

--- js.js
+++ js.js
@@ -2,5 +2,6 @@
 button = document.querySelector('button');
 
 button.addEventListener('click', function() {
-	alert('овечки думают о своём, овечном')
+	alert('овечки думают о своём, овечном ')
+	var emoji = String.fromCodePoint(0x1F621)
 })
